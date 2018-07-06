d3d93a95ba1df60f4160fa87e11da2c39d285d7a
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-07-06 10:18:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const bot = new Discord.Client();
 
 var prefix = ("/")
 var random = 0
-var pop = 0
-var snip = 0
-var fusil = 0
-var pisto = 0
-var kit = 0
-var bouffe = 0
-var shield = 0
 
 bot.on('ready', function() {
     bot.user.setUsername("GuildeRP")
@@ -21,51 +14,41 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
+
 if (message.content === "/coffre"){
     random();
 if (randnum == 1){
     message.channel.send(`Vous trouvez dans ce coffre un **pompe**`);
-    pop ++
 }
 if (randnum == 2){
     message.channel.send(`Vous trouvez dans ce coffre un **sniper**`);
-    snip ++
 }
 if (randnum == 3){
     message.channel.send(`Vous trouvez dans ce coffre un **fusil d'assaut**`);
-    fusil ++
 }
 if (randnum == 4){
     message.channel.send(`Vous trouvez dans ce coffre un **pistolet**`);
-    pisto ++
 }
 if (randnum == 5){
     message.channel.send(`Vous trouvez dans ce coffre un **Kit de soin**`);
-    kit ++
 }
 if (randnum == 6){
     message.channel.send(`Vous trouvez dans ce coffre un **kit de soin**`);
-    kit ++
 }
 if (randnum == 7){
     message.channel.send(`Vous trouvez dans ce coffre un **kit de soin**`);
-    kit ++
 }
 if (randnum == 8){
     message.channel.send(`Vous trouvez dans ce coffre un **kit de soin**`);
-    kit ++
 }
 if (randnum == 9){
     message.channel.send(`Vous trouvez dans ce coffre de la **nourriture**`);
-    bouffe ++
 }
 if (randnum == 10){
     message.channel.send(`Vous trouvez dans ce coffre de la **nourriture**`);
-    bouffe ++
 }
 if (randnum == 11){
     message.channel.send(`Vous trouvez dans ce coffre de la **nourriture**`);
-    bouffe ++
 }
 if (randnum == 12){
     message.channel.send(`Vous ne trouvez rien dans ce coffre`);
@@ -90,11 +73,9 @@ if (randnum == 18){
 }
 if (randnum == 19){
     message.channel.send(`Vous trouvez dans ce coffre de **l'équipement de protection**`);
-    shield ++
 }
 if (randnum == 20){
     message.channel.send(`Vous trouvez dans ce coffre de **l'équipement de protection**`);
-    shield ++
 }}
 }
 )
